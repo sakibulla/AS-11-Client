e62faf79e22c71edee7a5c2d61c5444916c82b1d
Sakib fahmid <hasanfahmidul2002@gmail.com>
2025-12-08 20:18:43 +0600

    Router setup done

---
+++ src/layout/RootLayout.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Outlet } from 'react-router';
+import Footer from '../pages/Shared/Footer/footer';
+import NavBar from '../pages/Shared/NavBar/NavBar';
+
+const RootLayout = () => {
+    return (
+        <div>
+            <NavBar></NavBar>
+            <Outlet></Outlet>
+            <Footer></Footer>
+            
+        </div>
+    );
+};
+
+export default RootLayout;
